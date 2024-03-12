61fa0ff5dec7c3f7440d874b7421fd485f5e138c
sfxoozore <oozore@seamfix.com>
2024-03-12 18:42:20 +0100

    paga frontend

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const App = () => {
 
-  
-  function initializeMonnifySDK() {
-    const { MonnifySDK } = window;
-    console.log("MonnifySDK", window.MonnifySDK);
-
-    if (MonnifySDK) {
-        MonnifySDK.initialize({
-            amount: 100,
-            currency: "NGN",
-            reference: String(new Date().getTime()),
-            customerFullName: "Damilare Ogunnaike",
-            customerEmail: "ogunnaike.damilare@gmail.com",
-            apiKey: "MK_TEST_TXR83EEGA8",
-            contractCode: "8116878414",
-            paymentDescription: "Lahray World",
-            metadata: {
-                "name": "Damilare",
-                "age": 45
-            },
-            onLoadStart: () => {
-                console.log("loading has started");
-            },
-            onLoadComplete: () => {
-                console.log("SDK is UP");
-            },
-            onComplete: (response) => {
-                console.log(response);
-            },
-            onClose: (data) => {
-                console.log(data);
-            }
-        });
-    } else {
-        console.error("MonnifySDK not available");
-    }
-
-  }
   return (
     <div>
-      <button onClick={initializeMonnifySDK}>Initialize Monnify SDK</button>
     </div>
   );
 };
